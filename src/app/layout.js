6f901ed3788bd952c2bc20b0cce97397d0b19721
rajib <shahrajib278@gmail.com>
2025-10-31 11:31:37 +0545

    fix: add meta icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,17 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import  Logo from '../../public/image/profile.jpg'
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
 export const metadata = {
   title: "Rajib Bikram Shah Portfolio",
-  description: "Generated From Rajib Bikram Shah Portfolio with using Next.js",
-    icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-32x32.png",
-    apple: "/",
+  description: "Generated from Rajib Bikram Shah Portfolio using Next.js",
+  icons: {
+    icon: "/image/profile.jpg", 
+    shortcut: "/favicon.png",   
+    apple: "/image/profile.jpg" 
   },
 };
 
