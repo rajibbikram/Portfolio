dd89f46b894828b7563106a96475d3865298148b
rajib <shahrajib278@gmail.com>
2025-11-02 17:40:33 +0545

    feat: meta google adseans

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import  Logo from '../../public/image/profile.jpg'
+import Logo from '../../public/image/profile.jpg'
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -9,15 +9,26 @@ export const metadata = {
   title: "rajibbikramshah",
   description: "Generated from Rajib Bikram Shah Portfolio using Next.js",
   icons: {
-    icon: "/image/profile.jpg", 
-    shortcut: "/favicon.png",   
-    apple: "/image/profile.jpg" 
+    icon: "/image/profile.jpg",
+    shortcut: "/favicon.png",
+    apple: "/image/profile.jpg"
+  },
+  other: {
+    "google-adsense-account": "ca-pub-5511490636642140",
   },
 };
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-5511490636642140"
+        />
+      </head>
       <body
         className={`antialiased`}
         style={{
